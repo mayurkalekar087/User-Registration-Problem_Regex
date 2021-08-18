262e42634d6cbce8652f0ae4751d049822868d7a
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-08-18 23:34:36 +0530

    feat:Clear all Email Samples

--- UserRegistrationRegex/Program.cs
+++ UserRegistrationRegex/Program.cs
@@ -10,6 +10,11 @@ namespace UserRegistrationRegex
             Patterns patterns = new Patterns();
             Console.WriteLine(patterns.validateFirstName("Mayur"));
             Console.WriteLine(patterns.validateLastName("Kalekar"));
+            Console.WriteLine(patterns.validateEmailId("abc-@yahoo.com"));
+            Console.WriteLine(patterns.validateEmailId("abc111@abc.com"));
+            Console.WriteLine(patterns.validateEmailId("abc.100@yahoo.com"));
+            Console.WriteLine(patterns.validateEmailId("abc-100@abc.net"));
+            Console.WriteLine(patterns.validateEmailId("abc.100@yahoo.com,"));
             Console.WriteLine(patterns.validateEmailId("mayurkalekar@gmail.com"));
             Console.WriteLine(patterns.validatePhoneNumber("8805291711"));
             Console.WriteLine(patterns.validatePassWord("Mayurkalekar@#12"));
